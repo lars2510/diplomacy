ee852ecd7d4353425363f313fedb35c551661e4b
lars schuettemeyer <lars.schuettemeyer@sinnerschrader.com>
2016-03-17 16:30:37 +0100

    new function scope

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,25 @@
-function createShip(xPos, yPos) {
-	var tank = document.createElement("div");
-	var offset = 16;
-	tank.classList.add("ship");
-	tank.style.top = (yPos - offset) + 'px';
-	tank.style.left = (xPos - offset) + 'px';
-	return tank;
-}
+;(function (){
+	function createShip(xPos, yPos) {
+		var tank = document.createElement("div");
+		var offset = 16;
+		tank.classList.add("ship");
+		tank.style.top = (yPos - offset) + 'px';
+		tank.style.left = (xPos - offset) + 'px';
+		return tank;
+	}
 
-function appendMapItem(xPos, yPos) {
-    var worldmap = document.getElementsByClassName('js_worldmap');
-    worldmap[0].appendChild(createShip(xPos, yPos));
-}
+	function appendMapItem(xPos, yPos) {
+	    var worldmap = document.getElementsByClassName('js_worldmap');
+	    worldmap[0].appendChild(createShip(xPos, yPos));
+	}
 
-function getClickPosition(e) {
-    appendMapItem(e.clientX, e.clientY)
-}
+	function getClickPosition(e) {
+	    appendMapItem(e.clientX, e.clientY)
+	}
 
-function init() {
-	var worldmap = document.getElementsByClassName('js_worldmap');
-	worldmap[0].addEventListener("click", getClickPosition);
-}
-
-init();
+	function init() {
+		var worldmap = document.getElementsByClassName('js_worldmap');
+		worldmap[0].addEventListener("click", getClickPosition);
+	}
+	init();
+})();
